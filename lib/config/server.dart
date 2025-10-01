97dde7cb7752b6c132802d1a52a1808c911109b3
Nguyễn Vũ <nguyenvanvu112003@gmail.com>
2025-10-01 15:57:39 +0700

    deploy be

--- lib/config/server.dart
+++ lib/config/server.dart
@@ -3,4 +3,4 @@ const String host = '192.168.1.142';
 // '172.16.11.8';
 const String port = '3000';
 
-const String baseUrl = 'http://$host:$port';
+const String baseUrl = 'https://app-hoc-tap.onrender.com';
