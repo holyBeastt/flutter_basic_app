a23359799c214f3784b241aaef74a84362bb3739
Nguyễn Vũ <nguyenvanvu112003@gmail.com>
2025-12-13 21:08:29 +0700

    ok otp login

--- lib/config/server.dart
+++ lib/config/server.dart
@@ -1,8 +1,5 @@
 const String host = '192.168.1.218';
-
-// '172.16.11.8';
 const String port = '3000';
 
-// const String baseUrl = 'https://app-hoc-tap.onrender.com';
-const String baseUrl = 'http://"$host":$port';
-
+// ✅ ĐÚNG
+const String baseUrl = 'http://$host:$port';
