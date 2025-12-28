5e414de1f86f274b8c3ec79a2d70e5ff7a16fcc4
Nguyễn Vũ <nguyenvanvu112003@gmail.com>
2025-12-28 20:44:34 +0700

    otp

--- lib/config/server.dart
+++ lib/config/server.dart
@@ -1,5 +1,5 @@
 /// Server configuration - Local development
-const String baseUrl = 'http://192.168.0.109:3000';
+const String baseUrl = 'http://192.168.1.39:3000';
 
 // Server configuration - Production
 // const String baseUrl = 'https://app-hoc-tap.onrender.com';
